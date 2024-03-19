42829129df63b9781238d23acd0c631ff4a0306f
marlokzy <marlon.r12325@gmail.com>
2024-03-19 15:42:15 -0300

    add do while code

---
+++ JS/JS I/aula27/index.js
@@ -0,0 +1,8 @@
+let velocidade = 0;
+
+do {
+    alert("A velocidade é: "+velocidade+" km/h");
+    velocidade += 20;
+} while(velocidade < 80);
+
+alert("A velocidade atual agora é: "+velocidade+ " km/h");
