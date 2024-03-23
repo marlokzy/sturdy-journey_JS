ce048379e79879bdd69a1aa8d7264b2ddef9c074
marlokzy <marlon.r12325@gmail.com>
2024-03-22 22:57:23 -0300

    update do while code

--- JS/JS I/aula28_ex1/index.js
+++ JS/JS I/aula28_ex1/index.js
@@ -1,23 +1,25 @@
+let option = "";
+
 do {
-    const option = parseFloat(prompt("Escolha uma das opções:\n1- .\n2 - ..\n3 - ...\n4 - ....\n5 - encerrar programa"));
+    option = prompt("Escolha uma das opções:\n1- .\n2 - ..\n3 - ...\n4 - ....\n5 - encerrar programa");
 
     switch(option){
-        case 1:
+        case "1":
             alert("a opção escolhida foi a 1");   
             break 
-        case 2:
+        case "2":
             alert("a opção escolhida foi a 2"); 
             break   
-        case 3:
+        case "3":
             alert("a opção escolhida foi a 3");   
             break
-        case 4:
+        case "4":
             alert("a opção escolhida foi a 4");    
             break
-        case 5:
+        case "5":
             alert("a opção escolhida foi a 5");
             break
     }
-} while(option !== 5);
+} while(option !== "5");
 
 alert("O sistema está sendo encerrado!!!");
