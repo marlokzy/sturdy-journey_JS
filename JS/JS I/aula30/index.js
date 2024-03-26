6691dfefb2b07f538dc3383a31f9f582ce47567c
marlokzy <marlon.r12325@gmail.com>
2024-03-26 16:08:20 -0300

    add for code

---
+++ JS/JS I/aula30/index.js
@@ -0,0 +1,9 @@
+// for(let indice = 0; indice <= 10; indice++){
+//     alert(indice);
+// }
+
+const nome = "Marlon";
+
+for(let indice = 0; indice <= nome.length; indice++){
+    console.log(nome[indice]);
+}
