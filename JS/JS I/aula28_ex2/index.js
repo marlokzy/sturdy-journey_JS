d1d4a3c458c248863b8cc754c79b3c7147950aee
marlokzy <marlon.r12325@gmail.com>
2024-03-26 15:17:06 -0300

    update ex2 do while code

--- JS/JS I/aula28_ex2/index.js
+++ JS/JS I/aula28_ex2/index.js
@@ -1,16 +1,20 @@
-do {
-    const money = parseFloat(prompt("Qual é a quantidade de dinheiro disponivel?"));
+// let money = parseFloat("");
+let option = "";
 
-    const option = alert("Saldo: "+money+"\n1 - Adicionar dinheiro\n2 - Remover dinheiro\n3 - Sair");
+let money = parseFloat(prompt("Qual é a quantidade de dinheiro disponivel?"));
+
+do {
+    
+    option = prompt("Saldo: "+money+"\n1 - Adicionar dinheiro\n2 - Remover dinheiro\n3 - Sair");
 
     switch (option) {
         case "1":
-                const remDin = prompt("Quantidade a ser adicionada:");
+                const remDin = parseFloat(prompt("Quantidade a ser adicionada:"));
 
                 money += remDin;
             break;        
         case "2":
-                const addDin = prompt("Quantidade a ser removida:");
+                const addDin = parseFloat(prompt("Quantidade a ser removida:"));
 
                 money -= addDin;
             break;        
