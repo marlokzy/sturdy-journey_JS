a912d1beb600d8aa0b14aac8e6a2b06a68544e36
marlokzy <marlon.r12325@gmail.com>
2024-04-04 12:46:08 -0300

    update ex2 do for

--- JS/JS I/aula31_ex2/index.js
+++ JS/JS I/aula31_ex2/index.js
@@ -1,5 +1,18 @@
-let word = prompt("Me informe a palavra para identificar se é um palíndromo!");
+const word = prompt("Me informe a palavra para identificar se é um palíndromo! (tudo em minúsculo)");
 
-for (let i = 0; i; i++) {
-    
+let invWord = "";
+
+// macarrão -> tem 8 letras mas na programação a primeira letra sempre começa pelo 0, ficando 7, por isso do menos 1, já que o método .lenght puxa o tamanho original da palavra que é 8
+for (let i = word.length - 1; i >= 0; i--) {
+    invWord += word[i];
 }
+
+// verificação para ver se a palavra é ou não é igual em ambos os sentidos
+if (word === invWord) {
+    alert("A palavra "+word+" é um palíndromo!\n\n"+
+    word+" == "+invWord);
+} else{
+    alert("A palavra "+word+" não é um palíndromo!\n\n"+
+    word+" !== "+invWord);
+}
+
