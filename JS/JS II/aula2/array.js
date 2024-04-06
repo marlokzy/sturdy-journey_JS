1b162af3256884b7b841990283b83175dda27e5f
marlokzy <marlon.r12325@gmail.com>
2024-04-05 22:13:11 -0300

    add code array

---
+++ JS/JS II/aula2/array.js
@@ -0,0 +1,29 @@
+const listaDeCompras = [];
+console.log(listaDeCompras);
+
+listaDeCompras [0] = 7;
+console.log(listaDeCompras);
+
+listaDeCompras [1] = 9;
+console.log(listaDeCompras);
+
+listaDeCompras [4] = 5;
+console.log(listaDeCompras);
+
+console.log(listaDeCompras[3]);
+
+////////////////////////////////////////
+
+const helloWorld = [];
+console.log(helloWorld);
+
+// helloWorld[1] = "oi";
+// console.log(helloWorld);
+
+helloWorld[0] = "Olá";
+console.log(helloWorld);
+
+helloWorld[1] = "Mundo!";
+console.log(helloWorld);
+
+// Brincando e treinando um pouco com arrays - o básico
